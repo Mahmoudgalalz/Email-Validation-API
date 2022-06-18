a46a4ad25d129046ef3ef7e0cd204e791e37c25a
Mahmoud Galal <ssss.aaa2002@gmail.com>
2022-06-18 18:23:46 +0200

    POST API done

--- index.js
+++ index.js
@@ -6,22 +6,38 @@ const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'))
-const pattern= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+app.use(express.static(__dirname + '/public'));
 
-let Email
+
+
+
 app.get('/',(req,res) =>{
-    res.sendFile(path.join(__dirname+"/main.html"));
+    res.sendFile(path.join(__dirname+"/views/main.html"));
 })
-app.get('/isvalid/:email', (req, res) => {
-    Email=req.params.email;
-    if(isValid(Email)){
-        console.log("Yes")
+app.post('/isvalid', (req, res) => {
+    const Email=req.body.email
+    if(!isValid(Email)){
+        res.status(500).json({
+            isValid:'NO'
+        })   
     }
+    else{
+    res.status(200).json({
+        isValid:'YES',
+        Domain:domain(Email)
+    })
+}
 });
 
 function isValid(Email){
+    const pattern= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     return pattern.test(Email)
+}
+
+//getting the email domain 
+function domain(Email){
+    let domain=Email.split("@")[1];
+    return domain.split('.')[0];
 }
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
